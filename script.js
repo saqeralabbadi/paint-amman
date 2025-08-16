cfdad1415bff76f19b95ba66871ffae4cc7cdbd6
saqeralabbadi <saqeralabbadi001@gmail.com>
2025-08-16 13:01:49 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// مثال: قائمة الهامبرغر للهواتف
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+});
